b807542549f28df9ca4c8218dcc35491d787a8fd
Nishant <me@nishantjha.org>
2022-10-29 13:49:55 -0700

    dark mode alignment viewer

--- components/Blast/GlobalAlignmentViz/GlobalAlignmentViz.tsx
+++ components/Blast/GlobalAlignmentViz/GlobalAlignmentViz.tsx
@@ -1,3 +1,5 @@
+import { Card } from "components/Card/Card";
+
 interface BlastResponseDatum {
   id: string;
   query_range: [number, number];
@@ -41,7 +43,7 @@ export const GlobalAlignmentViz = ({
   }));
 
   const bookend = (
-    <div className="text-align-center mx-auto my-2 w-full rounded-xl bg-blue-300 p-1 text-center text-xs font-semibold text-blue-800">
+    <div className="text-align-center mx-auto my-2 w-full rounded-xl bg-brand-300 p-1 text-center text-xs font-semibold text-brand-800">
       {sequenceName}
     </div>
   );
@@ -57,7 +59,7 @@ export const GlobalAlignmentViz = ({
         <div
           key={item.title}
           className={
-            "text-align-start my-2 cursor-pointer truncate rounded-xl bg-blue-500 p-1 pl-4 text-start text-xs text-white hover:bg-blue-700"
+            "text-align-start my-2 cursor-pointer truncate rounded-xl bg-brand-500 p-1 pl-4 text-start text-xs text-white hover:bg-brand-700"
           }
           style={{
             marginLeft: `${marginLeft}%`,
@@ -71,24 +73,22 @@ export const GlobalAlignmentViz = ({
   };
 
   return (
-    <section>
-      <div className="overflow-hidden border border-zinc-500 bg-zinc-700/30 shadow-2xl sm:rounded-lg md:mx-8">
-        <div className="border-b border-gray-200  px-4 py-4 sm:px-6">
-          <h3 className="space-between flex flex-row text-lg font-medium leading-6 text-blue-300">
-            <span className="flex-1">Alignment Viewer</span>
-          </h3>
-          <p className="mt-1 max-w-2xl text-sm text-zinc-400">
-            {results.length} Alignments
-          </p>
-        </div>
-        <div className="overflow-hidden px-4 py-5 sm:px-6">
-          <>
-            {bookend}
-            {alignmentItems.map((m) => middleAlignmentItem(m, sequenceLength))}
-            {bookend}
-          </>
-        </div>
+    <Card>
+      <header className="border-b border-gray-200 px-4 py-4 sm:px-6">
+        <h3 className="space-between flex flex-row text-lg font-medium leading-6 dark:text-brand-300 text-brand-600">
+          <span className="flex-1">Alignment Viewer</span>
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm text-zinc-400">
+          {results.length} Alignments
+        </p>
+      </header>
+      <div className="overflow-hidden px-4 py-5 sm:px-6">
+        <>
+          {bookend}
+          {alignmentItems.map((m) => middleAlignmentItem(m, sequenceLength))}
+          {bookend}
+        </>
       </div>
-    </section>
+    </Card>
   );
 };
