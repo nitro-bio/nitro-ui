5bf524608a8b6304a5485fe7b177c9ace597b945
digithed <nickrosenau1@gmail.com>
2022-11-04 09:31:20 -0400

    quick change.

--- components/ui/DropDown/DropDown.tsx
+++ components/ui/DropDown/DropDown.tsx
@@ -53,7 +53,7 @@ export const DropDown = ({ items, ...props }: DropDownProps) => {
           <Menu.Button className="focus-visible:ring-white inline-flex w-full justify-center rounded-md bg-blue-400 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75">
             Click Me
             <ChevronDownIcon
-              className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
+              className="ml-2 -mr-1 h-5 w-5 text-brand-200 hover:text-brand-100"
               aria-hidden="true"
             />
           </Menu.Button>
