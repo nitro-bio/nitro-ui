b023aa904e494e625d6447c3e2fe5496f2eb98a5
digithed <nickrosenau1@gmail.com>
2022-11-07 00:06:04 -0500

    Added alternate dark mode scheme for secondary intent.

--- src/components/ui/Shell/Shell.tsx
+++ src/components/ui/Shell/Shell.tsx
@@ -10,7 +10,7 @@ const shellStylesOuter = cva(
         primary:
           "border-solid border-2 border-zinc-200 bg-white dark:bg-zinc-900",
         secondary:
-          "border-solid border-2 border-brand-100 bg-brand-200 dark:bg-zinc-900",
+          "border-solid border-2 border-brand-100 bg-brand-300 dark:bg-zinc-900",
       },
     },
     defaultVariants: {
@@ -25,7 +25,7 @@ const shellStylesInner = cva(
     variants: {
       intent: {
         primary: "border-x-2 border-zinc-200 bg-white dark:bg-zinc-900",
-        secondary: "border-x-2 border-brand-100 bg-brand-200 dark:bg-zinc-900",
+        secondary: "border-x-2 border-brand-100 bg-brand-300 dark:bg-zinc-900",
       },
     },
     defaultVariants: {
@@ -62,7 +62,7 @@ const hrefStyles = cva("", {
   variants: {
     intent: {
       primary: "text-blue-300 dark:text-blue-300",
-      secondary: "text-brand-400 dark:text-blue-300",
+      secondary: "text-brand-700 dark:text-green-300",
     },
   },
   defaultVariants: {
@@ -70,17 +70,32 @@ const hrefStyles = cva("", {
   },
 });
 
-const subHeaderStyles = cva("", {
+const subHeaderStyles = cva("mt-6 text-base", {
   variants: {
     intent: {
       primary: "text-zinc-400 dark:text-zinc-400",
-      secondary: "text-brand-400 dark:text-zinc-100",
+      secondary: "text-brand-700 dark:text-green-300",
     },
   },
   defaultVariants: {
     intent: "primary",
   },
 });
+
+const hoverStyles = cva(
+  "absolute -inset-y-6 -inset-x-4 z-0 scale-95 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl",
+  {
+    variants: {
+      intent: {
+        primary: "bg-zinc-200/50 dark:bg-zinc-800/50",
+        secondary: "bg-brand-800/50 dark:bg-green-800/50",
+      },
+    },
+    defaultVariants: {
+      intent: "primary",
+    },
+  }
+);
 
 export interface ShellProps
   extends VariantProps<typeof shellStylesOuter>,
@@ -117,12 +132,14 @@ export interface UIMockupCardProps
     VariantProps<typeof textStyles>,
     VariantProps<typeof hrefStyles>,
     VariantProps<typeof textCardStyles>,
+    VariantProps<typeof hoverStyles>,
     UICard {}
 
 const sampleText =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
 
-const UISection = ({ name, intent, cards, ...props }: UISectionProps) => {
+const UISection = ({ ...props }: UISectionProps) => {
+  const { name, intent, cards } = props;
   return (
     <section className="mt-16 md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
       <div className="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
@@ -152,8 +169,8 @@ const UISection = ({ name, intent, cards, ...props }: UISectionProps) => {
     </section>
   );
 };
-const UIMockupCard = ({ intent, ...props }: UIMockupCardProps) => {
-  const { title, description, href } = props;
+const UIMockupCard = ({ ...props }: UIMockupCardProps) => {
+  const { title, description, intent, href } = props;
   return (
     <article className="flex flex-row md:items-baseline md:gap-8">
       <div className="max-w-xl" />
@@ -164,7 +181,7 @@ const UIMockupCard = ({ intent, ...props }: UIMockupCardProps) => {
               intent,
             })}`}
           >
-            <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-800/50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl"></div>
+            <div className={hoverStyles({ intent })}></div>
             <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
             <span className="relative z-10">{title}</span>
           </h2>
@@ -220,7 +237,7 @@ export const Shell = ({ intent, ...props }: ShellProps) => {
               >
                 About Me
               </h1>
-              <div className={`mt-6 text-base ${subHeaderStyles({ intent })}`}>
+              <div className={subHeaderStyles({ intent })}>
                 {"Some things about me."}
               </div>
             </header>
