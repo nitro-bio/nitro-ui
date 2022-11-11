daa3e6da85b56adecf3d42cf9c49d5e5968c6ecb
Nick Rosenau <nickrosenau1@gmail.com>
2022-11-10 19:58:23 -0500

    First pass at page links (#17)
    * First pass at links page. Also found small error in ListPage file.
    
    * First pass at links page. Also found small error in ListPage file.
    
    * Made pr changes.

---
+++ src/components/ui/PageLayouts/LinkPage.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { LinkPage } from "./LinkPage";
+
+export default {
+  title: "UI/LinkPage",
+  component: LinkPage,
+} as ComponentMeta<typeof LinkPage>;
+
+const sampleText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
+
+const entries = [
+  { title: "Link a", description: sampleText, href: "#" },
+  { title: "Link b", description: sampleText, href: "#" },
+  { title: "Link c", description: sampleText, href: "#" },
+];
+
+const Template: ComponentStory<typeof LinkPage> = () => {
+  return (
+    <LinkPage
+      entries={entries}
+      title={"About Me"}
+      subtitle={"Some things about me."}
+    />
+  );
+};
+
+export const LinkPageStory = Template.bind({});
