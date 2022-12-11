824ce9427ed72fbdcd145078bb1bbf3047bc6c79
Nishant <me@nishantjha.org>
2022-12-11 14:29:54 -0800

    default story has better layout

--- src/components/Gleipnir/Gleipnir.tsx
+++ src/components/Gleipnir/Gleipnir.tsx
@@ -11,17 +11,19 @@ export const Gleipnir = () => {
   const [currentGene, setCurrentGene] = useAtom(currentGeneAtom);
   const currentGeneIdx = GENES.findIndex((gene) => gene.id === currentGene?.id);
   return (
-    <div className="grid grid-cols-1 content-between md:grid-cols-3 ">
-      <div className="grid content-start p-8 md:col-span-1">
+    <div className="grid grid-cols-1 content-between bg-noir-50 md:grid-cols-2">
+      <div className="grid content-start bg-red-50 p-8">
         <Combobox
           options={GENES}
           onSelect={setCurrentGene}
           selectedOptionIdx={currentGeneIdx}
         />
       </div>
-      <GeneDetails />
-      <div className="row-span-2 grid content-center md:col-span-2">
+      <div className="row-span-2 grid content-center bg-blue-50">
         <MetabolicNetwork />
+      </div>
+      <div className="grid content-start bg-green-50 p-8">
+        <GeneDetails />
       </div>
     </div>
   );
