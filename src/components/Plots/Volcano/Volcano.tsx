386e6505d4f64ac780640c10db28dea76608226a
Nishant <me@nishantjha.org>
2023-02-04 10:14:18 -0800

    init Volcano

---
+++ src/components/Plots/Volcano/Volcano.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Card from "@ui/Card";
+import { bin } from "d3";
+import { useDebounce } from "@hooks/useDebounce";
+import { classNames } from "@utils/stringUtils";
+
+type Point = {
+  x: number;
+  y: number;
+  style?: string;
+};
+export interface VolcanoProps {
+  data: Point[];
+  pointClassName?: (b: Point) => string;
+  containerClassName?: string;
+}
+export const Volcano = ({
+  data,
+  pointClassName,
+  containerClassName,
+}: VolcanoProps) => {
+  return (
+    <div
+      className={classNames(
+        "volcano border h-[400px] w-[400px] relative",
+        containerClassName
+      )}
+    >
+      {data.map((d, i) => (
+        <div
+          key={i}
+          className={classNames(
+            "bg-brand-500 rounded-full h-2 w-2 absolute",
+            pointClassName && pointClassName(d)
+          )}
+          style={{ left: `${d.x}%`, bottom: `${d.y}%` }}
+        />
+      ))}
+    </div>
+  );
+};
