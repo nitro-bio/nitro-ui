30896e562e4894c29f54918d0f38555bf71956a6
Nishant <me@nishantjha.org>
2023-09-28 11:00:25 -0400

    simplify dropdown

--- src/components/ui/Dropdown/Dropdown.stories.tsx
+++ src/components/ui/Dropdown/Dropdown.stories.tsx
@@ -1,18 +1,80 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Dropdown } from "./Dropdown";
 
 export default {
   title: "UIElements/Dropdown",
   component: Dropdown,
-} as ComponentMeta<typeof Dropdown>;
+};
 
-const Template: ComponentStory<typeof Dropdown> = (args) => (
-  <div className="h-screen bg-white px-64 py-32 dark:bg-noir-800">
-    <Dropdown {...args} />
-  </div>
+function repeatArray<T>(arr: T[], n: number): T[] {
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(...arr);
+  }
+  return result;
+}
+
+const people = repeatArray(
+  [
+    "Wade Cooper",
+    "Arlene Mccoy",
+    "Devon Webb",
+    "Tom Cook",
+    "Tanya Fox",
+    "Hellen Schmidt",
+  ],
+  100
 );
 
-export const DropdownStory = Template.bind({});
-DropdownStory.args = {
-  intent: "primary",
+export const Default = () => {
+  return (
+    <div className="mx-auto flex min-h-screen w-full max-w-3xl justify-end">
+      <Dropdown
+        title="people"
+        onSelection={(option) => alert(option)}
+        menuItems={people}
+        menuButtonClassName="bg-noir-600 dark:bg-noir-600"
+        menuItemClassName="bg-noir-600 dark:bg-noir-600"
+        menuClassName="bg-noir-600 dark:bg-noir-600"
+      />
+    </div>
+  );
+};
+
+export const Variety = () => {
+  return (
+    <div className="mx-auto flex h-80 w-full max-w-3xl flex-col items-end gap-2">
+      <Dropdown
+        title="people"
+        onSelection={(option) => alert(option)}
+        menuItems={people}
+        menuButtonClassName="bg-red-600 dark:bg-red-600"
+        menuItemClassName="bg-red-600 dark:bg-red-600"
+        menuClassName="bg-red-600 dark:bg-red-600 h-20"
+      />
+      <Dropdown
+        title="people"
+        onSelection={(option) => alert(option)}
+        menuItems={people}
+        menuButtonClassName="bg-green-600 dark:bg-green-600"
+        menuItemClassName="bg-green-600 dark:bg-green-600"
+        menuClassName="bg-green-600 dark:bg-green-600 h-40"
+      />
+      <Dropdown
+        title="people"
+        onSelection={(option) => alert(option)}
+        menuItems={people}
+        menuButtonClassName="bg-blue-600 dark:bg-blue-600"
+        menuItemClassName="bg-blue-600 dark:bg-blue-600"
+        menuClassName="bg-blue-600 dark:bg-blue-600 h-60"
+      />
+      <Dropdown
+        title="people"
+        onSelection={(option) => alert(option)}
+        menuItems={people}
+        menuButtonClassName="bg-amber-600 dark:bg-amber-600"
+        menuItemClassName="bg-amber-600 dark:bg-amber-600"
+        menuClassName="bg-amber-600 dark:bg-amber-600 h-80"
+      />
+    </div>
+  );
 };
--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -1,89 +1,68 @@
-import { cva, VariantProps } from "class-variance-authority";
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { classNames } from "../../..";
+import RenderIfVisible from "react-render-if-visible";
 
-const menuDropdownButtonStyles = cva(
-  "focus-visible:ring-white inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75",
-  {
-    variants: {
-      intent: {
-        primary: "bg-brand-600 dark:bg-brand-400",
-        secondary: "bg-noir-500 dark:bg-noir-600",
-      },
-    },
-    defaultVariants: { intent: "primary" },
-  }
-);
-
-const menuItemsContainerStyles = cva(
-  "ring-black absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-opacity-5 focus:outline-none ",
-  {
-    variants: {
-      intent: {
-        primary: "bg-brand-600 dark:bg-brand-400 text-white",
-        secondary: "bg-noir-500 dark:bg-noir-600 dark:text-white text-white",
-      },
-    },
-    defaultVariants: {
-      intent: "primary",
-    },
-  }
-);
-
-const menuItemStyles = cva(
-  "group flex w-full items-center rounded-md px-2 py-2 text-sm dark:text-white",
-  {
-    variants: {
-      intent: {
-        primary: "bg-brand-600 dark:bg-brand-400 text-white",
-        secondary: "bg-noir-500 dark:bg-noir-600 dark:text-white",
-      },
-      active: {
-        true: "opacity-50 bg-opacity-50 bg-inherit",
-        false: "opacity-100 bg-opacity-100",
-      },
-    },
-    defaultVariants: {
-      active: false,
-    },
-  }
-);
-
-interface Props {
+export const MenuItem = ({
+  name,
+  className,
+  onClick,
+}: {
   name: string;
-}
-export interface MenuItemProps
-  extends VariantProps<typeof menuItemStyles>,
-    Props {}
-
-export const MenuItem = ({ ...props }: MenuItemProps) => (
-  <Menu.Item>
-    {({ active }) => (
-      <button className={menuItemStyles({ active })}>
-        <div className="mr-2 h-5 w-5" aria-hidden="true" />
-
-        {props.name}
-      </button>
-    )}
-  </Menu.Item>
+  className?: string;
+  onClick: () => void;
+}) => (
+  <RenderIfVisible defaultHeight={36}>
+    <Menu.Item>
+      {({ active }) => (
+        <button
+          className={classNames(
+            "group flex w-full items-center rounded-md px-2 py-2 text-sm dark:text-white",
+            active
+              ? "bg-inherit bg-opacity-50 opacity-50"
+              : "bg-opacity-100 opacity-100",
+            className
+          )}
+          onClick={onClick}
+        >
+          <div className="mr-2 h-5 w-5" aria-hidden="true" />
+          {name}
+        </button>
+      )}
+    </Menu.Item>
+  </RenderIfVisible>
 );
 
-const MenuItems = ["option 1", "option 2", "option 3"];
+export const Dropdown = ({
+  title,
+  menuClassName,
+  menuButtonClassName,
+  menuItemClassName,
+  menuItems,
+  onSelection,
+}: {
+  title: string;
+  menuClassName?: string;
+  menuItemClassName?: string;
+  menuButtonClassName?: string;
+  menuItems: string[];
+  onSelection: (selection: string) => void;
+}) => (
+  <Menu as="div" className={classNames("relative inline-block text-right")}>
+    <Menu.Button
+      className={classNames(
+        "focus-visible:ring-white inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75",
+        menuButtonClassName
+      )}
+    >
+      {title}
+      <ChevronDownIcon
+        className="-mr-1 ml-2 h-5 w-5 text-brand-200 hover:text-brand-100"
+        aria-hidden="true"
+      />
+    </Menu.Button>
 
-export type MenuContainerProps = VariantProps<typeof menuItemsContainerStyles>;
-
-export const Dropdown = ({ intent }: MenuContainerProps) => (
-  <Menu as="div" className="relative inline-block text-left text-right">
-    <div>
-      <Menu.Button className={menuDropdownButtonStyles({ intent })}>
-        Click Me
-        <ChevronDownIcon
-          className="-mr-1 ml-2 h-5 w-5 text-brand-200 hover:text-brand-100"
-          aria-hidden="true"
-        />
-      </Menu.Button>
-    </div>
     <Transition
       as={Fragment}
       enter="transition ease-out duration-100"
@@ -93,12 +72,20 @@ export const Dropdown = ({ intent }: MenuContainerProps) => (
       leaveFrom="transform opacity-100 scale-100"
       leaveTo="transform opacity-0 scale-95"
     >
-      <Menu.Items className={menuItemsContainerStyles({ intent })}>
-        <div className="px-1 py-1 ">
-          {MenuItems.map((name, index) => (
-            <MenuItem key={index} name={name} />
-          ))}
-        </div>
+      <Menu.Items
+        className={classNames(
+          "ring-black absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 overflow-y-auto  rounded-md p-1 text-white shadow-lg ring-1 ring-opacity-5 focus:outline-none",
+          menuClassName
+        )}
+      >
+        {menuItems.map((name, index) => (
+          <MenuItem
+            key={index}
+            name={name}
+            onClick={() => onSelection(name)}
+            className={menuItemClassName}
+          />
+        ))}
       </Menu.Items>
     </Transition>
   </Menu>
