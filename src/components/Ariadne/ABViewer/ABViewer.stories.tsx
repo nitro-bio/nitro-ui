13e88b11888bfb386adcbeb325be732e708680b5
Nishant <me@nishantjha.org>
2023-12-13 16:43:46 -0500

    HACK to fix build

--- src/components/Ariadne/ABViewer/ABViewer.stories.tsx
+++ src/components/Ariadne/ABViewer/ABViewer.stories.tsx
@@ -14,7 +14,6 @@ export default {
   },
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const Default = () => {
   const sequence =
     "ATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCG";
