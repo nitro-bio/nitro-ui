fec7b2e1143467da908d1b7c940d3f2af0e83a5f
Nishant Jha <me@nishantjha.org>
2024-03-08 11:19:55 -0800

    Plate v1 (#49)
    * [wip]
    
    * row/col labels
    
    * dark/light
    
    * toggle select by clicking wells/cols/rows

---
+++ src/components/Plate/Plate.tsx
@@ -0,0 +1,186 @@
+import { classNames } from "@utils/stringUtils";
+import { z } from "zod";
+
+const PlateSelectionSchema = z.object({
+  wells: z.set(z.number()),
+  className: z.string().optional(),
+});
+export type PlateSelection = z.infer<typeof PlateSelectionSchema>;
+
+export const Plate = ({
+  wells,
+  className,
+  selection,
+  setSelection,
+}: {
+  wells: 24 | 96 | 48 | 384;
+  selection: PlateSelection | null;
+  setSelection: (selection: PlateSelection) => void;
+  className?: string;
+}) => {
+  let gridClass: string;
+  const { rows, cols } = wellsToRowsCols(wells);
+  const rowLabels: string[] = Array.from({ length: rows }, (_, i) =>
+    (i + 1).toString(),
+  );
+  const colLabels: string[] = Array.from({ length: cols }, (_, i) =>
+    String.fromCharCode(65 + i),
+  );
+  const toggleWellInSelection = (well: number) => {
+    const newSelection = new Set(selection?.wells || []);
+    if (newSelection.has(well)) {
+      newSelection.delete(well);
+    } else {
+      newSelection.add(well);
+    }
+    setSelection({ wells: newSelection });
+  };
+  const toggleColumnInSelection = (col: number) => {
+    const newSelection = new Set(selection?.wells || []);
+    for (let row = 0; row < rows; row++) {
+      const well = row * cols + col;
+      if (newSelection.has(well)) {
+        newSelection.delete(well);
+      } else {
+        newSelection.add(well);
+      }
+    }
+    setSelection({ wells: newSelection });
+  };
+
+  const toggleRowInSelection = (row: number) => {
+    const newSelection = new Set(selection?.wells || []);
+    for (let col = 0; col < cols; col++) {
+      const well = row * cols + col;
+      if (newSelection.has(well)) {
+        newSelection.delete(well);
+      } else {
+        newSelection.add(well);
+      }
+    }
+    setSelection({ wells: newSelection });
+  };
+
+  switch (wells) {
+    case 24:
+      gridClass = "grid-cols-7 gap-4 ";
+      break;
+    case 48:
+      gridClass = "grid-cols-9 gap-4";
+      break;
+    case 96:
+      gridClass = "grid-cols-13 gap-1 ";
+      break;
+    case 384:
+      gridClass = "grid-cols-25 gap-1 ";
+      break;
+    default:
+      throw new Error("Invalid number of wells");
+  }
+
+  return (
+    <>
+      <div
+        className={classNames(
+          "grid gap-2 rounded-md rounded-r-3xl border border-noir-800 py-4 pr-4 dark:border-noir-200",
+
+          wells > 96 && "px-4",
+          gridClass,
+          className,
+        )}
+      >
+        <div className="col-span-full col-start-2 grid grid-cols-subgrid ">
+          {colLabels.map((colLabel) => (
+            <button
+              key={colLabel}
+              className={classNames(
+                "flex items-end justify-center",
+                wells > 96 && "break-all px-1 text-[0.6rem]",
+                "border-b border-l border-r border-noir-300 pb-1 text-noir-600 dark:border-noir-500 dark:text-noir-300",
+                "hover:bg-noir-200 hover:text-brand-500 dark:text-noir-600 hover:dark:bg-noir-600 hover:dark:text-brand-200",
+              )}
+              onClick={() => {
+                toggleColumnInSelection(colLabels.indexOf(colLabel));
+              }}
+            >
+              {colLabel}
+            </button>
+          ))}
+        </div>
+        <div
+          className={classNames(
+            "col-span-1 grid grid-cols-subgrid gap-1 ",
+            wells > 96 && "content-between py-1",
+            "text-noir-600 dark:text-noir-300",
+          )}
+        >
+          {rowLabels.map((rowLabel) => (
+            <button
+              key={rowLabel}
+              onClick={() => {
+                toggleRowInSelection(rowLabels.indexOf(rowLabel));
+              }}
+              className={classNames(
+                "mx-auto h-full px-1",
+                wells > 96 && "text-[0.6rem]",
+                "border-b border-r border-t border-noir-300 pb-1 pr-1 text-noir-600 dark:border-noir-500 dark:text-noir-300",
+                "hover:bg-noir-200 hover:text-brand-500 dark:text-noir-600 hover:dark:bg-noir-600 hover:dark:text-brand-200",
+              )}
+            >
+              {rowLabel}
+            </button>
+          ))}
+        </div>
+        <div className="col-span-full col-start-2 grid grid-cols-subgrid gap-2 ">
+          {Array.from({ length: wells }).map((_, i) => (
+            <button
+              key={i}
+              className={classNames(
+                "group my-auto flex aspect-square min-h-px min-w-px cursor-pointer items-center justify-center rounded-full ",
+                "transition-all duration-300 ease-in-out",
+                "border border-noir-800 dark:border-noir-200",
+                "text-noir-300 hover:scale-110 hover:bg-noir-200 hover:text-brand-500 dark:text-noir-600 hover:dark:bg-noir-600 hover:dark:text-brand-200 ",
+                selection?.wells.has(i) &&
+                  "bg-brand-200 text-noir-800 hover:bg-brand-400 hover:text-white dark:bg-brand-600 dark:text-white hover:dark:bg-brand-700",
+                selection?.wells.has(i) && selection.className,
+              )}
+              onClick={() => {
+                toggleWellInSelection(i);
+                console.log("clicked");
+              }}
+            >
+              <div
+                className={classNames(wells > 96 && "opacity-0")}
+                title={indexToExcelCell(i, wells)}
+              >
+                {indexToExcelCell(i, wells)}
+              </div>
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+const wellsToRowsCols = (wells: 24 | 96 | 48 | 384) => {
+  switch (wells) {
+    case 24:
+      return { rows: 4, cols: 6 };
+    case 48:
+      return { rows: 6, cols: 8 };
+    case 96:
+      return { rows: 8, cols: 12 };
+    case 384:
+      return { rows: 16, cols: 24 };
+    default:
+      throw new Error("Invalid number of wells");
+  }
+};
+
+const indexToExcelCell = (index: number, wells: 24 | 96 | 48 | 384) => {
+  const { cols } = wellsToRowsCols(wells);
+  const row = Math.floor(index / cols);
+  const col = index % cols;
+  return `${String.fromCharCode(65 + col)}${row + 1}`;
+};
