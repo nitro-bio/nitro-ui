d4730bc18583e582091042fba7bc2f252855f07a
Nishant <me@nishantjha.org>
2024-03-10 15:26:14 -0700

    switch to cmdk for combobox

--- src/components/ui/Combobox/Combobox.tsx
+++ src/components/ui/Combobox/Combobox.tsx
@@ -1,148 +1,130 @@
-import { cva, VariantProps } from "class-variance-authority";
-import { Fragment, useEffect, useState } from "react";
-import { Combobox as HeadlessCombobox, Transition } from "@headlessui/react";
-import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { classNames } from "@utils/stringUtils";
-import RenderIfVisible from "react-render-if-visible";
+import { Command } from "cmdk";
+import { useState } from "react";
 
 interface Props {
-  options: ComboboxOption[];
+  search: string;
+  setSearch: (search: string) => void;
+  sections: ComboboxSection[];
   onSelect: (option: ComboboxOption) => void;
-  selectedOptionIdx: number;
-  optionsContainerClassName?: string;
+  loading?: boolean;
+  loadingText?: string;
+  placeholder?: string;
+  emptyMessage?: React.ReactNode;
+  inputClassName?: string;
+  itemClassName?: string;
+  groupClassName?: string;
+  listClassName?: string;
+  commandClassName?: string;
 }
 
-const comboboxStyles = cva();
-export interface ComboboxProps
-  extends VariantProps<typeof comboboxStyles>,
-    Props {}
-
-interface ComboboxOption {
+export interface ComboboxOption {
   id: string;
   label: string;
 }
+export interface ComboboxSection {
+  id: string;
+  label: string;
+  options: ComboboxOption[];
+}
 
 export function Combobox({
-  options,
-  selectedOptionIdx,
+  search,
+  setSearch,
+  loading,
+  loadingText,
+  sections,
   onSelect,
-  optionsContainerClassName,
-}: ComboboxProps) {
-  const [selected, setSelected] = useState(options[0]);
-  useEffect(
-    function syncSelectedOption() {
-      if (selectedOptionIdx >= 0) {
-        const selectedOption = options[selectedOptionIdx];
-
-        setSelected(selectedOption);
-      }
-    },
-    [selectedOptionIdx, options],
-  );
-
-  const [query, setQuery] = useState("");
-
-  const filteredOptions: ComboboxOption[] =
-    query === ""
-      ? options
-      : options.filter((opt) =>
-          opt.label
-            .toLowerCase()
-            .replace(/\s+/g, "")
-            .includes(query.toLowerCase().replace(/\s+/g, "")),
-        );
-
+  placeholder,
+  emptyMessage,
+  inputClassName,
+  itemClassName,
+  groupClassName,
+  listClassName,
+  commandClassName,
+}: Props) {
+  const [open, setOpen] = useState(false);
   return (
-    <HeadlessCombobox
-      value={selected}
-      onChange={(e) => {
-        setSelected(e);
-        onSelect(e);
-      }}
+    <Command
+      label="Command Menu"
+      className={classNames(
+        "block text-sm font-medium leading-6 text-noir-900",
+        commandClassName,
+      )}
     >
-      <div className="relative mt-1">
-        <div
+      <Command.Input
+        className={classNames(
+          "w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-noir-900 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset dark:bg-noir-800 dark:text-noir-100 sm:text-sm sm:leading-6",
+          inputClassName,
+        )}
+        placeholder={placeholder}
+        value={search}
+        onValueChange={setSearch}
+        onFocus={() => setOpen(true)}
+      />
+      {loading && (
+        <Command.Loading
+          className={classNames("text-brand-800 dark:text-brand-200")}
+        >
+          {loadingText ? loadingText : "Loading.."}
+        </Command.Loading>
+      )}
+      {open && (
+        <Command.List
           className={classNames(
-            "focus-visible:ring-brand-300 relative w-full cursor-default overflow-hidden rounded-lg text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 dark:border dark:border-brand-300 sm:text-sm",
+            "ring-black z-10 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-opacity-5 focus:outline-none dark:bg-noir-800 dark:text-noir-100 sm:text-sm",
+            listClassName,
           )}
         >
-          <HeadlessCombobox.Input
-            className={classNames(
-              "w-full border-none bg-white py-2 pl-3 pr-10 text-sm leading-5 text-noir-900 focus:ring-0 dark:bg-noir-800 dark:text-brand-100",
-            )}
-            displayValue={(opt: ComboboxOption) => opt.label}
-            onChange={(event) => setQuery(event.target.value)}
-          />
-          <HeadlessCombobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-            <ChevronUpDownIcon
-              className="h-5 w-5 text-noir-400"
-              aria-hidden="true"
-            />
-          </HeadlessCombobox.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          afterLeave={() => setQuery("")}
-        >
-          <HeadlessCombobox.Options
-            className={classNames(
-              "ring-brand-300 absolute z-10 mt-1 w-full  overflow-auto rounded-md bg-white bg-white py-1 text-base shadow-2xl ring-1 ring-opacity-5 focus:outline-none dark:bg-noir-800 dark:text-noir-100 sm:text-sm",
-              optionsContainerClassName,
-            )}
+          <Command.Empty
+            className={classNames(itemClassName, "py-2 pl-3 pr-9")}
           >
-            {filteredOptions.length === 0 && query !== "" ? (
-              <div className="relative cursor-default select-none px-4 py-2 text-noir-700">
-                Nothing found.
-              </div>
-            ) : (
-              filteredOptions.map((opt) => (
-                <RenderIfVisible key={opt.id} defaultHeight={36}>
-                  <HeadlessCombobox.Option
-                    className={({ active }) =>
-                      classNames(
-                        "relative cursor-default select-none py-2 pl-10 pr-4",
-                        active
-                          ? "bg-brand-600 text-white dark:text-noir-100"
-                          : "text-noir-900 dark:text-noir-100",
-                      )
-                    }
-                    value={opt}
-                    onSelect={() => setSelected(opt)}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={classNames(
-                            "block truncate",
-                            selected ? "font-medium" : "font-normal",
-                          )}
-                        >
-                          {opt.label}
-                        </span>
-                        {selected ? (
-                          <span
-                            className={classNames(
-                              "absolute inset-y-0 left-0 flex items-center pl-3",
-                              active
-                                ? "text-white dark:text-noir-800"
-                                : "text-brand-600",
-                            )}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </HeadlessCombobox.Option>
-                </RenderIfVisible>
-              ))
-            )}
-          </HeadlessCombobox.Options>
-        </Transition>
-      </div>
-    </HeadlessCombobox>
+            {emptyMessage ? <>{emptyMessage}</> : "No results found"}
+          </Command.Empty>
+
+          {sections.map((section) => {
+            return (
+              <Command.Group
+                key={section.id}
+                heading={
+                  <div className="py-2">
+                    <label
+                      className={classNames(
+                        "ml-3 border-b border-brand-400 py-2 pr-8 text-brand-600 dark:border-brand-400 dark:text-brand-400",
+                        groupClassName,
+                      )}
+                    >
+                      {section.label}
+                    </label>
+                  </div>
+                }
+              >
+                {section.options.map((option) => {
+                  return (
+                    <Command.Item
+                      key={option.id}
+                      onSelect={() => {
+                        onSelect(option);
+                        setSearch(option.label);
+                        // get a weird setState error if we don't do wait to close
+                        setTimeout(() => setOpen(false), 100);
+                      }}
+                      className={classNames(
+                        "relative cursor-default select-none py-2 pl-3 pr-9",
+                        "hover:bg-noir-100 dark:hover:bg-noir-700",
+                        "text-noir-900 dark:text-noir-100",
+                        itemClassName,
+                      )}
+                    >
+                      {option.label}
+                    </Command.Item>
+                  );
+                })}
+              </Command.Group>
+            );
+          })}
+        </Command.List>
+      )}
+    </Command>
   );
 }
