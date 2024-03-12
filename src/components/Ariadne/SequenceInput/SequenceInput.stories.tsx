963c772df88c25286bc007b1a68eba3141762cfa
Nishant <me@nishantjha.org>
2024-03-12 10:30:46 -0700

    [wip]

--- src/components/Ariadne/SequenceInput/SequenceInput.stories.tsx
+++ src/components/Ariadne/SequenceInput/SequenceInput.stories.tsx
@@ -1,15 +1,4 @@
-import { generateRandomAnnotations } from "@Ariadne/storyUtils";
-import { getAnnotatedSequence, getStackedAnnotations } from "@Ariadne/utils";
 import { Card } from "@ui/Card";
-import { useMemo, useState } from "react";
-
-import type {
-  AA,
-  AnnotatedAA,
-  AnnotatedNucl,
-  AriadneSelection,
-  Nucl,
-} from "@Ariadne/types";
 import { SequenceInput } from "@Ariadne/SequenceInput";
 
 export default {
