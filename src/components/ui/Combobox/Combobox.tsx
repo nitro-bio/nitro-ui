7fca0452e5e254f65b77e9f37a897a6ee34cb9e0
Nishant <me@nishantjha.org>
2024-03-13 08:20:21 -0700

    always render commandlist bugfix

--- src/components/ui/Combobox/Combobox.tsx
+++ src/components/ui/Combobox/Combobox.tsx
@@ -69,62 +69,59 @@ export function Combobox({
           {loadingText ? loadingText : "Loading.."}
         </Command.Loading>
       )}
-      {open && (
-        <Command.List
-          className={classNames(
-            "ring-black z-10 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-opacity-5 focus:outline-none dark:bg-noir-800 dark:text-noir-100 sm:text-sm",
-            listClassName,
-          )}
-        >
-          <Command.Empty
-            className={classNames(itemClassName, "py-2 pl-3 pr-9")}
-          >
-            {emptyMessage ? <>{emptyMessage}</> : "No results found"}
-          </Command.Empty>
+      <Command.List
+        className={classNames(
+          "ring-black z-10 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-opacity-5 focus:outline-none dark:bg-noir-800 dark:text-noir-100 sm:text-sm",
+          listClassName,
+          open ? "visible" : "invisible",
+        )}
+      >
+        <Command.Empty className={classNames(itemClassName, "py-2 pl-3 pr-9")}>
+          {emptyMessage ? <>{emptyMessage}</> : "No results found"}
+        </Command.Empty>
 
-          {sections.map((section) => {
-            return (
-              <Command.Group
-                key={section.id}
-                heading={
-                  <div className="py-2">
-                    <label
-                      className={classNames(
-                        "ml-3 border-b border-brand-400 py-2 pr-8 text-brand-600 dark:border-brand-400 dark:text-brand-400",
-                        groupClassName,
-                      )}
-                    >
-                      {section.label}
-                    </label>
-                  </div>
-                }
-              >
-                {section.options.map((option) => {
-                  return (
-                    <Command.Item
-                      key={option.id}
-                      onSelect={() => {
-                        onSelect(option);
-                        setSearch(option.label);
-                        // get a weird setState error if we don't do wait to close
-                        setTimeout(() => setOpen(false), 100);
-                      }}
-                      className={classNames(
-                        "relative cursor-default select-none py-2 pl-3 pr-9",
-                        "hover:bg-noir-100 dark:hover:bg-noir-700",
-                        "text-noir-900 dark:text-noir-100",
-                        itemClassName,
-                      )}
-                    >
-                      {option.label}
-                    </Command.Item>
-                  );
-                })}
-              </Command.Group>
-            );
-          })}
-        </Command.List>
-      )}
+        {sections.map((section) => {
+          return (
+            <Command.Group
+              key={section.id}
+              heading={
+                <div className="py-2">
+                  <label
+                    className={classNames(
+                      "ml-3 border-b border-brand-400 py-2 pr-8 text-brand-600 dark:border-brand-400 dark:text-brand-400",
+                      groupClassName,
+                    )}
+                  >
+                    {section.label}
+                  </label>
+                </div>
+              }
+            >
+              {section.options.map((option) => {
+                return (
+                  <Command.Item
+                    key={option.id}
+                    onSelect={() => {
+                      onSelect(option);
+                      setSearch(option.label);
+                      // get a weird setState error if we don't do wait to close
+                      setTimeout(() => setOpen(false), 100);
+                    }}
+                    className={classNames(
+                      "relative cursor-default select-none py-2 pl-3 pr-9",
+                      "hover:bg-noir-100 dark:hover:bg-noir-700",
+                      "text-noir-900 dark:text-noir-100",
+                      itemClassName,
+                    )}
+                  >
+                    {option.label}
+                  </Command.Item>
+                );
+              })}
+            </Command.Group>
+          );
+        })}
+      </Command.List>
     </Command>
   );
 }
